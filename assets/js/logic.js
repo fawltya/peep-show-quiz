14554cc542cbfd41515f4c2127bec83bbcd05021
Seamus Barnett <seamus0689@gmail.com>
2023-01-25 22:11:17 +0000

    Start of quiz functionality

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,3 +6,49 @@
 // The quiz should end when all questions are answered or the timer reaches 0.
 // When the game ends, it should display their score and give the user the ability to save their initials and their score
 
+var startButton = $('#start')
+var startScreen = $('#start-screen')
+var endScreen = $('#end-screen')
+var questions = $('#questions')
+var question = $('question-title')
+var choices = $('#choices')
+var time = $('#time')
+
+// function timer(timerLength) {
+//     // var timeAmount = setInterval(timerLength, 1000);
+//     time.append(timerLength)
+// }
+
+// timer(0)
+// setInterval(timer(200000000000), 1000)
+
+
+
+// var counter = 0;
+
+// for (counter < 100 ) {
+//     var interval = setInterval(function () {
+//         counter++;
+//       }, 1000);
+// }
+
+
+// // time.text(interval)
+// console.log(interval)
+
+
+
+// to stop the counter
+// clearInterval(inteval);
+
+function quiz() {
+    questions.removeClass('hide');
+    startScreen.addClass('hide')
+}
+
+startButton.on("click", function(event) {
+    event.preventDefault();
+    quiz();
+})
+
+console.log(questionArray[0]);
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,27 +1,27 @@
-var questions = [
+var questionArray = [
     {
         question: "1. Which Berkshire town did Super Hans accidentally run to?",
         choices: ["Leeds", "Reading", "Windsor", "Slough"],
-        answer: 3,
+        answer: 2,
     },
     {
         question: "2. What do Mark and Jeremy call themselves as a duo?",
         choices: ["Meremy", "The El Dude Brothers", "Jark", "The troublesome twosome"],
-        answer: 2,
+        answer: 1,
     },
     {
         question: "3. Where did Jeremy and Mark go to university?",
         choices: ["Dartmouth", "York", "Manchester", "Brighton"],
-        answer: 1,
+        answer: 0,
     },
     {
         question: "4. What does Jeremy make Super Hans change the name of his band to?",
         choices: ["The Hair Blair Bunch", "Mama's Kumquat", "Coming Up For Blair", "Danny Dyer's Chocolate Homunculus"],
-        answer: 4,
+        answer: 3,
     },
     {
         question: "5. Where does Mark propose to Sophie and attempt to propose to Dobby",
         choices: ["The Lake District", "The Quantocks", "Apollo House", "Paris"],
-        answer: 2,
+        answer: 1,
     },    
 ]
